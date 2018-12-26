b4a37e487e73ea028b7c660521efaaa39701269e
Heloise Taillet <ht2394@columbia.edu>
2018-12-26 05:42:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,13 +9,16 @@ def array_with_two_elements
   return array
 end
 
-def first_element
+def first_element(array)
+  array[0]
 end
 
-def third_element
+def third_element(array)
+  array[2]
 end
 
-def last_element
+def last_element(array)
+  array[-1]
 end
 
 def first_element_with_array_methods
